d082c9eb7bbd3751b3f04c0403e093aa0945c373
Abid <abdur.abid@madetech.com>
2025-05-19 15:42:24 +0100

    update deploy

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-#^8y^&i&(1+%ci9iy&nvur=^qedz(-oj&1*&*^9(xwg(i@aggt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'endgame.eu-west-2.elasticbeanstalk.com'
+]
 
 
 # Application definition
