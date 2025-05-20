11d5384e80674ebbd386c9f33811b4f4e667258e
Abid <abdur.abid@madetech.com>
2025-05-20 14:06:10 +0100

    update workflow

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # this is where collectstatic will copy files
+STATIC_ROOT = '/tmp/staticfiles'
 
 # Optional (helps with DRF assets when DEBUG=False)
 STATICFILES_DIRS = [
