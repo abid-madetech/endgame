e4555598dc4f6c1152dbe05e7111f2954155dffd
Abid <abdur.abid@madetech.com>
2025-05-30 12:19:07 +0100

    add theme table and column to ksb

--- core/models.py
+++ core/models.py
@@ -8,11 +8,16 @@ class KSBType(models.Model):
     def __str__(self):
         return self.name
 
+class Theme(models.Model):
+    id = models.SmallIntegerField(primary_key=True)
+    name = models.CharField(max_length=255, unique=True)
+
 class KSB(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200, unique=True)
     description = models.TextField(unique=True)
     ksb_type = models.ForeignKey(KSBType, on_delete=models.CASCADE)
+    theme = models.ForeignKey(Theme, on_delete=models.SET_NULL, null=True)
     completed = models.BooleanField(default=False)
     last_updated = models.DateTimeField(auto_now=True)
 
